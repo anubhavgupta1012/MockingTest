1fa4499fae4755e41dd41d8554e2a0e77b7cae30
Dheeraj <anubhavgupta1012@gmail.com>
2023-05-07 02:31:33 +0530

    Removed @SpringBootTest From Unit Test Cases

--- src/test/java/com/test/MockingTest/controller/ProductControllerTest.java
+++ src/test/java/com/test/MockingTest/controller/ProductControllerTest.java
@@ -5,7 +5,6 @@ import com.test.MockingTest.service.ProductService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -14,7 +13,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 
-@SpringBootTest
 class ProductControllerTest {
 
     private ProductService productService;
